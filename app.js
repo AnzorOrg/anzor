04ada55e4b91c80ff7de8715b16ea9f323a2514c
Dhruv Vajpeyi <vajpeyi2@gmail.com>
2016-02-20 01:29:19 -0600

    User model and login

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 'use strict';
 
 var express = require('express');
+var mongoose = require('mongoose');
+const crypto = require('crypto')
+var User = require(__dirname+'/server/models/User');
 
+mongoose.connect('mongodb://130.211.185.244:27017/mydb');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.status(200).sendFile(__dirname+'/webapp/dist/index.html')
+  res.status(200).sendFile(__dirname+'/webapp/dist/index.html');
+});
+
+
+app.post('/api/login', function(req, res) {
+	User.findOne({email:req.email}).exec(function(err, user){
+		if(err)
+			res.json({email:'Not found'});
+		else
+			res.json(user);
+	});
 });
 
 if (module === require.main) {
