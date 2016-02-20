869e9eaa6a82889185a2742600685c5b9f5a0f59
Ryan Kelch <ryannkelch@gmail.com>
2016-02-20 13:58:26 -0600

    Added create user method

--- webapp/src/app/user-data/user-data.service.ts
+++ webapp/src/app/user-data/user-data.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from 'angular2/core'
+import {Http, Response, Headers} from 'angular2/http'
 import {User} from './user'
 
 
@@ -7,13 +8,27 @@ export class UserDataService {
 	
 	public user: User;
 	
-	constructor() {
+	constructor(private _http: Http) {
 
 	}
 
-	createUser = function (firstName, lastName, email){
+	createUser = function (firstName, lastName, email, password){
 		this.user = new User(firstName, lastName, email)
 
+		var headers = new Headers()
+		headers.append('Content-Type','application/json')
+
+		var body = {
+			method: 'POST',
+			firstName: firstName,
+			lastName: lastName,
+			email: email,
+			password: password
+		}
+
+		this._http.post('/api/createUser/', JSON.stringify(body), {headers: headers}).subscribe(function (res){
+			console.log(res)
+		})
 	}
 
 	getFirstName = function (){
--- webapp/src/app/user-data/user.ts
+++ webapp/src/app/user-data/user.ts
@@ -3,10 +3,12 @@ export class User {
 	firstName: string;
 	lastName: string;
 	email: string;
+	password: string
 
 	constructor (firstName, lastName, email){
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 	}
+
 }
