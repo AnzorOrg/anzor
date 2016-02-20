c70ee02a1e6eda3a95d94ce0c344fb06588dfbc5
Dhruv Vajpeyi <vajpeyi2@gmail.com>
2016-02-20 01:59:13 -0600

    create user

--- app.js
+++ app.js
@@ -3,18 +3,36 @@
 var express = require('express');
 var mongoose = require('mongoose');
 const crypto = require('crypto')
-var User = require(__dirname+'/server/models/User');
+var bodyParser = require('body-parser')
+var User = require(__dirname+'/server/models/user.js');
 
 mongoose.connect('mongodb://130.211.185.244:27017/mydb');
+
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+extended: true
+}));
 
 app.get('/', function(req, res) {
   res.status(200).sendFile(__dirname+'/webapp/dist/index.html');
 });
 
+app.post('/api/createUser', function(req, res){
+	var hash = crypt.createHash('sha256').update(req.password).digest('base64');
+	req.body.password = hash;
+	var newUser = new User(req.body);
+	User.findOne({email: req.body.email}).exec(function(err, user){
+		if(err)
+			newUser.save(function(err){
+				res.status(200).end();
+			})
+	});
+});
 
 app.post('/api/login', function(req, res) {
-	User.findOne({email:req.email}).exec(function(err, user){
+	var hash = crypt.createHash('sha256').update(req.password).digest('base64');
+	User.findOne({email:req.email, password:hash}).exec(function(err, user){
 		if(err)
 			res.json({email:'Not found'});
 		else
