8300dbe5b0e7bb89108c86babd0a2eaa8ada1a01
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2016-02-20 20:48:53 -0600

    Made navbar snazzy

--- webapp/src/app/navbar/navbar.component.ts
+++ webapp/src/app/navbar/navbar.component.ts
@@ -1,7 +1,48 @@
 import {Component} from 'angular2/core';
+import {Router} from 'angular2/router';
+import {UserDataService} from '../user-data/user-data.service';
 
 @Component({
 	selector: 'navbar',
 	templateUrl: './app/navbar/navbar.html'
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+
+	constructor(
+		private _router: Router,
+		private _userDataService: UserDataService
+	){};
+
+	showDropdown = function(dropdown) {
+		dropdown.setAttribute('hidden', 'false');
+	}
+
+	hideDropdown = function(dropdown) {
+		dropdown.setAttribute('hidden', 'true');
+	}
+
+	signIn = function(){
+		//	Show sign in modal
+		console.log('Sign in modal!');
+	};
+
+	signOut = function(){
+		var self = this;
+
+		if(this._userDataService.signOut != null)
+			this._userDataService.signOut(function(){self.userSignedOut()});
+	};
+
+	userSignedIn = function(){
+		this._router.navigate(['Dashboard']);
+	};
+
+	userSignedOut = function() {
+		this._router.navigate(['Welcome']);
+	};
+
+	profileClicked = function() {
+		console.log('Settings!');
+	}
+
+}
